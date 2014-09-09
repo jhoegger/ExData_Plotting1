8cf813e7cddc4b69e418a18b90d6bc5e0afd61fa
JHoegger <JHoegger@hotmail.com>
2014-09-09 00:06:05 -0700

    update to plot2

--- plot2.r
+++ plot2.r
@@ -1,10 +1,9 @@
 ## Plot2 ##
 
-
 library(utils)
 library(dplyr)
 setwd("C:/Projects/Coursera-DS/4.Explore/Project")
-if (!file.exists("power_data.zip")) { 
+if (!file.exists("PowerData.zip")) { 
     download.file(url="https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", 
         destfile="PowerData.zip") 
 
@@ -14,22 +13,19 @@ filename <- "household_power_consumption.txt"
 ##only read in the first 100,000 rows of data
 data <- read.table(unz("PowerData.zip", filename), header=T, quote="\"", sep=";", na.strings="?", nrows=100000) 
 
-## Only get Feb1 and Feb2 data
-data <- data[data$Date >= as.Date("2007-02-01") & data$Date <= as.Date("2007-02-02"), ] 
-
 ## Convert the date to a date object 
 data$Date <- as.Date(data$Date, format="%d/%m/%Y") 
+
+## Only get Feb1 and Feb2 data
+data <- data[data$Date >= as.Date("2007-02-01") & data$Date <= as.Date("2007-02-02"), ] 
  
 ## Create a new Date/Time combined column
 data$DateTime <- strptime(paste(data$Date, data$Time), format="%Y-%m-%d %H:%M:%S") 
-
-
-##png(filename = "Plot2.png", width=480, height=480, bg="transparent") 
- 
 
 ## Create plot and save a png file
 
 plotFile <- "C:/Projects/Coursera-DS/4.Explore/Project/plot2.png"
 png(filename = plotFile, width = 480, height = 480, units = "px", pointsize = 12,  bg = "white", res = NA, family = "", restoreConsole = TRUE)
+##windows()
 plot(y=data$Global_active_power, x=data$DateTime, ylab="Global Active Power (killowatts)", xlab="",type="l") 
 dev.off()
--- plot3.r
+++ plot3.r
@@ -1,29 +1,34 @@
 ## Plot2 ##
 
 
-
+library(utils)
 library(dplyr)
 setwd("C:/Projects/Coursera-DS/4.Explore/Project")
+if (!file.exists("PowerData.zip")) { 
+    download.file(url="https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", 
+        destfile="PowerData.zip") 
+
+}
+
 filename <- "household_power_consumption.txt"
 ##only read in the first 100,000 rows of data
-data <-read.csv(filename, sep = ";", header = TRUE, nrows=100000)
-DT <- tbl_df(data)
-##filter to just 2/1/2007 & 2/2/2007
-DTFeb <-filter(DT, Date == as.Date("2/1/2007", "%m/%d/%Y") | Date == as.Date("2/2/2007", "%m/%d/%Y"))
+data <- read.table(unz("PowerData.zip", filename), header=T, quote="\"", sep=";", na.strings="?", nrows=100000) 
 
-DTFeb[DateTime] <- paste(DTFeb$Date, DTFeb$Time, Sep=" ")
+## Convert the date to a date object 
+data$Date <- as.Date(data$Date, format="%d/%m/%Y") 
 
-##change the date to by of type Date
-DT$Date <-  as.Date(DT$Date, "%d/%m/%Y")
+## Only get Feb1 and Feb2 data
+data <- data[data$Date >= as.Date("2007-02-01") & data$Date <= as.Date("2007-02-02"), ] 
+ 
+## Create a new Date/Time combined column
+data$DateTime <- strptime(paste(data$Date, data$Time), format="%Y-%m-%d %H:%M:%S") 
 
-DTFeb$Global_active_power <- as(DTFeb$Global_active_power, "character")
-##change DTFeb$Global_active_power to be numeric
-DTFeb$Global_active_power <- as(DTFeb$Global_active_power, "numeric")
-##plot Hist
-plotFile <- "C:/Projects/Coursera-DS/4.Explore/Project/plot2.png"
+## Create plot and save a png file
+plotFile <- "C:/Projects/Coursera-DS/4.Explore/Project/plot3.png"
 png(filename = plotFile, width = 480, height = 480, units = "px", pointsize = 12,  bg = "white", res = NA, family = "", restoreConsole = TRUE)
-windows()
+plot (y=data$Sub_metering_1, x=data$DateTime, ylab="Energy sub metering", xlab="",type="l", col="black")
+lines(y=data$Sub_metering_2, x=data$DateTime, col="red", type="l") 
+lines(y=data$Sub_metering_3, x=data$DateTime, col="blue", type="l") 
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lty=c(1,1)) 
 
-hist(DTFeb$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)", breaks=15,  xlim = c(0, 6))
-savePlot(plotfile, type = "png", device = dev.cur())
 dev.off()
